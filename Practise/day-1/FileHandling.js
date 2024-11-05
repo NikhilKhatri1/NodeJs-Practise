92688f10dbdf02800aa4d70c5847daef9eabd377
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2024-11-05 23:59:34 +0530

    .

--- Practise/day-1/FileHandling.js
+++ Practise/day-1/FileHandling.js
@@ -1,12 +1,23 @@
 const fs = require("fs");
 
+
+// async method
 const createFile = fs.writeFile(
     "example.txt",
-    "Hello Node.js File is Created using writeFile()",
+    "Hello! Node.js File is Created using writeFile() and read by using readFile()",
+    // if i write new content in this it will replace the content
     (err) => {
         if (err) throw err;
         console.log('File Created Successfully! and Data is Written!')
     });
 
+const readFile = fs.readFile(
+    "example.txt",
+    "utf-8",
+    (err, data) => {
+        if (err) throw err;
+        console.log(data)
+    })
 
-module.exports = { createFile };
+
+module.exports = { createFile, readFile };
--- Practise/server.js
+++ Practise/server.js
@@ -1,11 +1,14 @@
 // const greet = require("./day-1/greeting.js")  // store value in a variable
 
 const { sayHello, sayGoodbye } = require("./day-1/greeting.js")  // destructuring the import file
-const { createFile } = require('./day-1/FileHandling.js')
+const { createFile, readFile } = require('./day-1/FileHandling.js')
 sayHello("Alice")
 console.log("==============================");
 sayGoodbye("Alice")
 
 console.log("+++++++++++++++++++++++++++++++")
 
-console.log(createFile)
+// console.log(createFile)
+
+console.log(readFile);
+
