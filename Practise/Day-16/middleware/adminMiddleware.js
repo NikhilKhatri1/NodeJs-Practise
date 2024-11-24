e572c56d86ddcbc8f4920548bfae38b8e0db8105
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2024-11-24 22:59:13 +0530

    admin auth

---
+++ Practise/Day-16/middleware/adminMiddleware.js
@@ -0,0 +1,18 @@
+
+
+// check for user is admin or not
+
+const isAdminUser = (req, res, next) => {
+   
+    if (req.userInfo.role !== "admin") {
+       
+        return res.status(403).json({
+            success: false,
+            message: 'Access Denied, not a admin..'
+        });
+   
+    }
+    next()
+};
+
+module.exports = isAdminUser;
--- Practise/Day-16/routes/admin-routes.js
+++ Practise/Day-16/routes/admin-routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const authMiddleware = require('../middleware/authMiddleware.js')
+const adminMiddleware = require('../middleware/adminMiddleware.js')
 const router = express.Router();
 
-router.get('/welcome', authMiddleware, (req, res) => {
+router.get('/welcome', authMiddleware, adminMiddleware, (req, res) => {
+    
     res.json({ message: "Welcome to Admin Page" })
+
 });
 
 module.exports = router;
