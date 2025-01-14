4cbff48a6c86b1bea5c859c46c44b1380b34c9f5
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2025-01-14 22:03:26 +0530

    .

--- Practise/Day-3/wrapper-explorer.js
+++ Practise/Day-3/wrapper-explorer.js
@@ -1,6 +1,10 @@
 console.log("__filename This is wrapper explorer ", __filename);
 console.log("__dirname This is wrapper explorer ", __dirname);
 
-module.exports.greet = function (name) {
-    console.log("Hello, ", name);
+const greet = (name)=>{
+    console.log(`Hello ${name}`)
+}
+
+module.exports = {
+    greet
 }
