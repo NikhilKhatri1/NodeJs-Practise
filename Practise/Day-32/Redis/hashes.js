677feb6c8637fec6e330ba99813dc1c269c27bbe
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2025-02-07 03:41:24 +0530

    Redis Data Structure Set method

--- Practise/Day-32/Redis/hashes.js
+++ Practise/Day-32/Redis/hashes.js
@@ -15,9 +15,28 @@ const hashing = async () => {
         client.connect();
         console.log('Connected to Redis');
 
-        // hash ->
+        // hashes -> HSET, HGET, HGETALL, HDEL
+        // HSET -> set the multiple values
+        // HGET -> to get the value
+        // HGETALL -> to get all the value
+        // HDEL -> to delete the value
 
-
+        await client.hSet("Product:1", {
+            name: 'Product 1',
+            description: 'Description of Product',
+            rating: 5,
+        });
+        // particular detail of product
+        const getProductRating = await client.hGet("Product:1", "rating");
+        console.log("Rating of Product = ", getProductRating);
+        //all details of product
+        const getAllDetail = await client.hGetAll("Product:1");
+        console.log("All Detail of Product = ", getAllDetail);
+        // delete the detail of product
+        const deleteDetail = await client.hDel("Product:1", "rating");
+        console.log("No. of Delete Detail = ", deleteDetail);
+        // updated Detail
+        console.log("Updated detail = ", await client.hGetAll("Product:1"));
 
 
     } catch (error) {
